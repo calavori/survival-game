e66e78fdecb039c1b08526a51d0d154caf4868f4
calavori <calavori1998@gmail.com>
2020-09-11 11:54:25 +0700

    Add collider for terrain

---
+++ Assets/Editor/UpdateableDataEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UpdatetableData), true)]
+public class UpdateableDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        UpdatetableData data = (UpdatetableData)target;
+
+        if (GUILayout.Button("Update"))
+        {
+            data.NotifyOfUpdatedValues();
+        }
+    }
+}
---
+++ Assets/Scripts/Terrain/Data/TerrainData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class TerrainData : UpdatetableData
+{
+    public float meshHeightMutiplier;
+    public AnimationCurve meshHeightCurve;
+    public bool useFallOff;
+}
