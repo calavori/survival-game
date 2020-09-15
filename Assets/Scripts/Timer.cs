c3b6cd5749544bd1452aaaf44ada8293f9222449
calavori <calavori1998@gmail.com>
2020-09-15 09:52:27 +0700

    Fix timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,40 +5,39 @@ using UnityEngine;
 [ExecuteAlways]
 public class Timer : MonoBehaviour
 {
-    public static float startTime;
-    [Range(1, 10)]
     public float timeAccel;
-    public float timer;
 
-    float lastFrameTime
+    // Time.delta after accelerate
+    public float lastFrameTime
     {
         get
         {
-            return Time.deltaTime* timeAccel;
-        }
-    }
-    float seconds
-    {
-        get
-        {
-            return timer % 60;
+            return Time.deltaTime / timeAccel;
         }
     }
 
-    float days
-    {
-        get
-        {
-            return seconds % 86400;
-        }
-    }
 
+    public float dayNightTimer;
+    public float oneDaysTimer;
+    public float daysCount;
+
+    public enum DayState { Day, Night };
+    public DayState dayState;
+
+    private const float _FramesUntilEnd12h = 43200 * 60;         // Count down second to end 12h
+    private const float _FramesUntilEndDay = 86400 * 60;         // Count down second to end a day = 24h
 
     // Start is called before the first frame update
     void Start()
     {
         // Todo Load start time in save file
-        timer = startTime;
+        // If new game, game start at 12h at noon
+        if (dayNightTimer == 0 && daysCount == 0 && oneDaysTimer == 0)
+        {
+            dayState = DayState.Day;
+            dayNightTimer = _FramesUntilEnd12h;
+            oneDaysTimer = _FramesUntilEnd12h;
+        }
     }
 
     // Update is called once per frame
@@ -46,8 +45,70 @@ public class Timer : MonoBehaviour
     {
         if (Application.isPlaying)
         {
-            timer += lastFrameTime;
+            // If the first day in new game
+            if (daysCount == 0)
+            {
+                StartTimersInFirstDay();
+            }
+            // If other than first day
+            else
+            {
+                RunTimersInNormal();
+            }
         }
+    }
+
+    void StartTimersInFirstDay()
+    {
+        oneDaysTimer -= (1 * timeAccel);
+        
+        // When over 18h in game
+        if (oneDaysTimer <= _FramesUntilEnd12h / 2)
+        {
+            // Switch on night state at 18h
+            if (dayState == DayState.Day)
+            {
+                dayState = DayState.Night;
+            }
+            // Start the day/night timer at 18h
+            dayNightTimer -= (1 * timeAccel);
+        }
+
+        // When 24 night in game
+        if (oneDaysTimer <= 0)
+        {
+            daysCount = 1;
+            oneDaysTimer = _FramesUntilEndDay;
+        }
+    }
+
+    void RunTimersInNormal()
+    {
+        // Count timer
+        oneDaysTimer -= (1 * timeAccel);
+        dayNightTimer -= (1 * timeAccel);
+
+        // Swith day/night state when day/night timer is run out and reset timer
+        if (dayNightTimer <= 0)
+        {
+            dayNightTimer = _FramesUntilEnd12h;
+            if (dayState == DayState.Day)
+            {
+                dayState = DayState.Night;
+            }
+            else
+            {
+                dayState = DayState.Day;
+            }
+        }
+
+        // Reset day timer and days count after a day
+        if (oneDaysTimer <= 0)
+        {
+            oneDaysTimer = _FramesUntilEndDay;
+            daysCount += (1 * timeAccel);
+        }
+
     }
 
 }
